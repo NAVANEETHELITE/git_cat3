20414ceda13bd50e0cabe53c4dd7c513d430cdd1
NAVANEETHELITE <navaneethanselvakumar@gmail.com>
2024-06-09 20:04:10 +0530

    Modified dog file, get updated from cat

--- cat.py
+++ cat.py
@@ -1,1 +1,6 @@
 print('Round1 : CAT vs GIT')
+
+print('Change not reflected in loal git')
+
+print("Editied in github editor")
+
--- dog.py
+++ dog.py
@@ -1,1 +1,3 @@
 print("Round2 : DOG vs GIT")
+print("Deleted change in main to get it back from cat_branch")
+
